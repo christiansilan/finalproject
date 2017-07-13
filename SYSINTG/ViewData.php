ec1393fbb3b2804055ccda7bae890018e624af07
Denmark <denmarktayong01@yahoo.com>
2017-07-13 19:33:19 +0800

    Fixed ViewData.php
    Added all enhancements required
    
    Added error handling functionalities

--- SYSINTG/ViewData.php
+++ SYSINTG/ViewData.php
@@ -22,12 +22,12 @@
 <form name="Login" method="post" action="" >
 	<input type="submit" name="logout" value="Logout"><br><br>
 	<input type="submit" name="all" value="View All Data"> <br>
+	Other view types: <br>
 	
-	<input type="checkbox" name="sortType" value="getAgeRange">
+	<input type="checkbox" name="viewType[]" value="viewAgeRange">
 	View Age range from: <input type="text" name="age1" size=2> to: <input type="text" name="age2" size=2>
-	<input type="submit" name="ageRange" value="Go"><br>
-	
-	<input type="checkbox" name="sortType" value="sortByUniversity">
+	<br>
+	<input type="checkbox" name="viewType[]" value="viewByUniversity">
 	Sort by University:
 	<select name="sortedUniversity">
 		<option value="Ateneo De Manila University">Ateneo De Manila University</option>
@@ -38,9 +38,9 @@
 		<option value="University of Santo Tomas">University of Santo Tomas</option>
 		<option value="University of the Philippines">University of the Philippines</option>
 	</select>
-	<input type="submit" name="univSort" value="Go"> <br>
-	
-	<input type="checkbox" name="sortType" value="computeTotalStudents">
+	<br>
+	<input type="submit" name="viewTypeSubmit" value="Go"> <br>
+	<!--
 	Compute total students in university:
 	<select name="computedUniversity">
 		<option value="Ateneo De Manila University">Ateneo De Manila University</option>
@@ -51,25 +51,100 @@
 		<option value="University of Santo Tomas">University of Santo Tomas</option>
 		<option value="University of the Philippines">University of the Philippines</option>
 	</select>
-	<input type="submit" name="univCompute" value="Go"><br>
+	<input type="submit" name="univCompute" value="Compute"><br> -->
 </form>
 <?php
 	$mode=1;
+	$error="";
+	$message="";
 	if(isset($_POST['logout'])) {
 		header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/logout.php");
 	}
 	else if (isset($_POST['all'])) {
 		$mode=1;
+		$message.="The list of students from all Universities";
 	}
-	else if (isset($_POST['ageRange'])) {
-		$mode=2;
+	else if (isset($_POST['viewTypeSubmit']) ) {
+		if (isset($_POST['viewType'])) {
+			$viewType = $_POST['viewType'];
+			$arr = array();
+			for($i = 0; $i < 2; $i++) {
+				array_push($arr, 0);
+			}
+			for ($i = 0; $i <  count($_POST['viewType']); $i++) {
+				 if($viewType[$i] == "viewAgeRange" ) {
+					 $arr[0] = 1;
+				 }
+				 else if($viewType[$i] == "viewByUniversity" ) {
+					 $arr[1] = 1;
+				 }
+			}
+			
+			//Checking
+			if($arr[0] == 1 && $arr[1] == 1){
+				$mode=4;
+				$message.="The list of students ages ".$_POST['age1']." to ".$_POST['age2']." from ".$_POST['sortedUniversity'];
+			}
+			else if($arr[1] == 1) {
+				$mode=3;
+				$message.="The list of students from ".$_POST['sortedUniversity'];
+			}
+			else if($arr[0] == 1) {
+				if(isset($_POST['age1']) && isset($_POST['age2'])) {
+					if((!ctype_digit($_POST['age1']) || !ctype_digit($_POST['age2'])) || ($_POST['age1'] > $_POST['age2'])) {
+						$error.="Error! Invalid input in age fields!";
+						$message.="Viewing the list of students from all universities instead.";
+					}
+					else {
+						$mode=2;
+						$message.="The list of students ages ".$_POST['age1']." to ".$_POST['age2'];
+					}
+				}
+				else {
+					$error.="Error! Age fields left blank!";
+					$message.="Viewing the list of students from all universities instead.";
+				}
+			}
+			else 
+				$mode=1;
+			
+			if($mode == 3 || $mode == 4) {
+				include 'dbconnection.php';
+				$sqlCount = "SELECT COUNT(university) as 'totalStudents' FROM student WHERE university='{$_POST['sortedUniversity']}'";
+				$resultCount = $conn->query($sqlCount);
+				$rowCount = $resultCount->fetch_assoc();
+				$totalStudents = $rowCount['totalStudents'];
+				$message.="<br>Total ".$_POST['sortedUniversity']." Students: ".$totalStudents;
+			}
+		}
+		else {
+			$error.="Error! No checkbox checked!";
+			$message.="Viewing the list of students from all universities instead.";
+		}
 	}
-	else if (isset($_POST['univSort'])) {
-		$mode=3;
+	else {
+		$mode=1;
+		$message.="The list of students from all Universities";
 	}
+	/*
 	else if (isset($_POST['univCompute'])) {
-		$mode=4;
-	}
+		$mode=5;
+		include 'dbconnection.php';
+		$sqlCount = "SELECT COUNT(university) as 'totalStudents' FROM student WHERE university='{$_POST['computedUniversity']}'";
+		$resultCount = $conn->query($sqlCount);
+		$rowCount = $resultCount->fetch_assoc();
+		$totalStudents = $rowCount['totalStudents'];
+		$message.="The list of students from ".$_POST['sortedUniversity'];
+		$message.="<br>Total ".$_POST['sortedUniversity']." Students: ".$totalStudents;
+	}*/
+?>
+
+<?php 
+echo "<p>";
+if($error!="") {
+	echo "<br><font size='5' color='red'>".$error."<font size='3'>";
+}
+echo "<br><font size='5' color='black'>".$message."</p><font size='3'>";
 ?>
 <table id="studentData" class="display" cellspacing="0" width="100%">
     <thead>
@@ -93,14 +168,12 @@
 			$sql = "SELECT studentId, firstName, surname, birthday, university FROM student WHERE university='{$_POST['sortedUniversity']}'";
 		}
 		else if ($mode==4) {
-			$sql = "SELECT studentId, firstName, surname, birthday, university FROM student WHERE university='{$_POST['computedUniversity']}'";
-			
-			$sqlCount = "SELECT COUNT(university) as 'totalStudents' FROM student WHERE university='{$_POST['computedUniversity']}'";
-			$resultCount = $conn->query($sqlCount);
-			$rowCount = $resultCount->fetch_assoc();
-			$totalStudents = $rowCount['totalStudents'];
-			echo "Total Students: ".$totalStudents;
+			$sql = "SELECT studentId, firstName, surname, birthday, university FROM student WHERE university='{$_POST['sortedUniversity']}' AND FLOOR(DATEDIFF(CURDATE(), birthday)/365.24) >= {$_POST['age1']} AND FLOOR(DATEDIFF(CURDATE(), birthday)/365.24) <= {$_POST['age2']}";
 		}
+		/*
+		else if ($mode==5) {
+			$sql = "SELECT studentId, firstName, surname, birthday, university FROM student WHERE university='{$_POST['computedUniversity']}'";
+		}*/	
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 			// output data of each row
--- SYSINTG/dbconnection.php
+++ SYSINTG/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost:3306";
 $username = "root";
-$password = "";
+$password = "1234";
 $dbname = "sysintg";
 
 // Create connection
